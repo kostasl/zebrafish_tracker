f9e446e630149d4d61ab17943831e2f58227e3c8
kostasl <costaslag@gmail.com>
2015-12-23 16:59:47 +0000

    Added mp4 Files

--- main.cpp
+++ main.cpp
@@ -142,7 +142,7 @@ int main(int argc, char *argv[])
 
     QString invideoname = "*.mpg";
     unsigned int istartFrame = 0;
-    QStringList invideonames =QFileDialog::getOpenFileNames(0, "Select timelapse video to Process", qApp->applicationDirPath(), "Video file (*.mpg *.avi)", 0, 0);
+    QStringList invideonames =QFileDialog::getOpenFileNames(0, "Select timelapse video to Process", qApp->applicationDirPath(), "Video file (*.mpg *.avi *.mp4)", 0, 0);
 
     //Show Video list to process
     cout << "Video List To process:" << endl;
