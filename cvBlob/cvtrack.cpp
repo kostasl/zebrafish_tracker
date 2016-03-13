8ba6f32a8dfa40726a3613bdc7eb1d55cceaff3a
kostasl <costaslag@gmail.com>
2016-03-13 14:29:28 +0000

    Repaired issue with collapsing tracks due to effectiveDistance starting from 0 - Set Dist for blob->track max to 3xthDistance

--- cvBlob/cvtrack.cpp
+++ cvBlob/cvtrack.cpp
@@ -227,7 +227,7 @@ namespace cvb
         {
           CvTrack* t = T(j); //Fetch The blob to examine ROI
           CvBlob* b = B(i); //Fetch The blob to examine ROI
-          C(i, j) = distantBlobTrack(b,t) < min( 2*(t->effectiveDisplacement+2),3*thDistance);
+          C(i, j) = distantBlobTrack(b,t) < min( 2*(t->effectiveDisplacement+5),3*thDistance);
           //if (C(i, j) < thDistance  ) //< thDistance (t->effectiveDisplacement + 5)
           if(C(i, j))
           {
