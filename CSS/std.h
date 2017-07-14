c9ab592ff679f70a8cc3731633be133417f0c13d
kostas lago <costaslag@gmail.com>
2017-07-14 17:09:36 +0100

    Added CSS smottihing Fnct

---
+++ CSS/std.h
@@ -0,0 +1,24 @@
+/*
+ *  std.h
+ *  CurveMatching
+ *
+ *  Created by Roy Shilkrot on 11/28/12.
+ *  Copyright 2012 MIT. All rights reserved.
+ *
+ */
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <set>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/video/video.hpp>
