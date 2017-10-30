d909de0b91e02bed42f8b3452a2b8a7b33b3cd00
kostas lago <costaslag@gmail.com>
2017-10-30 16:41:28 +0000

    Added R plot for live Eye

---
+++ Rplots/LivePlot.r
@@ -0,0 +1,8 @@
+layout(matrix(1:2,ncol=1))
+while(T){
+    data=read.table("AutoSet_26-10-17_WTLiveFedRoti_167_008_tracks_1.csv",header=T);
+    plot(data$frameN[(nrow(data)-350):nrow(data)],(data$EyeLDeg-data$EyeRDeg)[(nrow(data)-350):nrow(data)],type='l',ylim=c(-15,80));
+    lines(data$frameN[(nrow(data)-350):nrow(data)],data$EyeLDeg[(nrow(data)-350):nrow(data)],col='green');
+    lines(data$frameN[(nrow(data)-350):nrow(data)],data$EyeRDeg[(nrow(data)-350):nrow(data)],col='red');
+    plot(data$frameN[(nrow(data)-350):nrow(data)],(data$AngleDeg)[(nrow(data)-350):nrow(data)],type='l',ylim=c(0,360));
+}
