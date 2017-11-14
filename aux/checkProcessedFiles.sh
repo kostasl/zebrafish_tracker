f7c948b8d59ea613e883b9fef3118dc0b49d7ddc
kostasl <costaslag@gmail.com>
2017-11-14 18:18:38 +0000

    Util Shows only Unprocessed Videos

--- aux/checkProcessedFiles.sh
+++ aux/checkProcessedFiles.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+echo "Kostas Lagogiannis 2017 - Check Track Videos Progress Utility"
+echo "Finds Which videos at param dir do not have tracked .csv file in the current directory . ie finds which videos have not been processed yet"
+
+
+DATDIR=$1
+VIDDIR=$2
+
+echo $VIDDIR
+echo $DATDIR
 
 rm datafileslist.txt
 rm vidfilelist.txt
-find . -maxdepth 1 | sed 's,^[^/]*/,,' | sed s/\.[^\_]*$// | sed s/\.[^\_]*$// | sort | uniq -u > datafileslist.txt
-find /media/kostasl/extStore/ExpData/zebrafish_preycapturesetup/AutoSet_02-11-17 -maxdepth 1 | sed 's,^[^/]*/,,' | sed s/\.[^\.]*$// | sed 's/.*\///' | sort | uniq -u > vidfilelist.txt
-diff datafileslist.txt vidfilelist.txt
+find $DATDIR -maxdepth 1 | sed 's,^[^/]*/,,' | sed s/\.[^\_]*$// | sed s/\.[^\_]*$// | sort | uniq > datafileslist.txt
+find $VIDDIR -maxdepth 1 | sed 's,^[^/]*/,,' | sed s/\.[^\.]*$// | sed 's/.*\///' | sort | uniq > vidfilelist.txt
+diff datafileslist.txt vidfilelist.txt --changed-group-format='%>'
+
+echo "**file listed are the video file names in source video directory for which a datafile is missing.** "
+echo "--------------------"
