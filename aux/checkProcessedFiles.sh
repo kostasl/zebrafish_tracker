4e8a1c13990cf188282f4386da94a938538c477e
kostasl <costaslag@gmail.com>
2017-12-02 21:43:32 +0000

    Increased tail scan angle

--- aux/checkProcessedFiles.sh
+++ aux/checkProcessedFiles.sh
@@ -25,7 +25,7 @@ echo "--------------------------"
 grep -f unprocessedfiles.txt vidfilesfullpath.txt > VidFilesToProcess.txt
 
 lines=`wc -l VidFilesToProcess.txt | cut -f1 -d' '`
-procsize=60
+procsize=30
 num=0
 ##Break It Down To Processing Files
 
