bf76c9ede679efa40d2d9ac7592cf0f5a0755525
Kostas Lago <costaslag@gmail.com>
2018-11-26 14:59:11 +0000

    Working On Detailed Analysis Per Fish

--- Rplots/DataLabelling/main_AnalysisPerFish.r
+++ Rplots/DataLabelling/main_AnalysisPerFish.r
@@ -3,6 +3,16 @@
 ### KOstasl 13-08-18
 
 
+#strProcDataFileName <-paste("setn-12","-HuntEvents-SB-ALL",sep="") ##To Which To Save After Loading
+#strProcDataFileName <- paste("setn14-HuntEventsFixExpID-SB-Updated-Merged",sep="") ##To Which To Save After Loading
+strProcDataFileName <- paste("setn15-HuntEvents-SB-Updated-Merged") ##To Which To Save After Loading
+message(paste(" Loading Hunt Event List to Process... "))
+#load(file=paste(strDatDir,"/LabelledSet/",strProcDataFileName,".RData",sep="" )) ##Save With Dataset Idx Identifier
+datHuntEventAllGroupToLabel <- readRDS(file=paste(strDatDir,"/LabelledSet/",strProcDataFileName,".rds",sep="" ))
+##<- datHuntEvent
+groupsList <- c("DL","NL","LL") ##unique(datHuntEventAllGroupToLabel$groupID)
+str_FilterLabel <- "UnLabelled"
+
 ##Histogram of Success Doesn t Show Any obvious differences
 #layout(matrix(c(1,2,3), 3, 1 ,byrow=TRUE))
 #hist(datFishSuccessRate[datFishSuccessRate$groupID == "LL",]$Success,breaks = seq(0,45,3),ylim=c(0,50),main="LL",col="#0000FFAA")
@@ -14,8 +24,11 @@
 #datFishSuccessRate[,"Success"] <- as.numeric(datFishSuccessRate[,"Success"])
 #
 
+datFishSuccessRate <- getHuntSuccessPerFish(datHuntEventAllGroupToLabel)
+
 vScoreIdx <- ((datFishSuccessRate[,"Success"]-datFishSuccessRate[,"Fails"])/(datFishSuccessRate[,"Success"]+datFishSuccessRate[,"Fails"]))
 vScoreIdx[is.nan(vScoreIdx) ] <- 0
+
 
 ##How Many Fish From Each Group Have A Score Higher Than :
 tblSuccessDist <- table(datFishSuccessRate[datFishSuccessRate$Success > 0, ]$groupID,datFishSuccessRate[datFishSuccessRate$Success > 0, ]$Success )
