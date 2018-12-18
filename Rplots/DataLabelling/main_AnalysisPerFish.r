09723b374cd7f209112f9784b38bb2e2619a7399
Kostas Lago <costaslag@gmail.com>
2018-12-18 16:12:33 +0000

    Added efficiency/Hunt Power CDF showing LL distribution shifts

--- Rplots/DataLabelling/main_AnalysisPerFish.r
+++ Rplots/DataLabelling/main_AnalysisPerFish.r
@@ -34,7 +34,7 @@ datFishSuccessRateMerged <- cbind(datFishSuccessRate,markUnTrackable=data.frame(
 
 names(datFishSuccessRateMerged)[5:7] <- c("markUnTrackable","markTracked","notTracked") ##Set Field Names - notTracked : Have not been detailed retracked yet
 
-vScoreIdx        <- ((datFishSuccessRate[,"Success"]-datFishSuccessRate[,"Fails"])/(datFishSuccessRate[,"Success"]+datFishSuccessRate[,"Fails"]))
+vScoreIdx        <- ((datFishSuccessRate[,"Success"]*datFishSuccessRate[,"Success"])/(datFishSuccessRate[,"Success"]+datFishSuccessRate[,"Fails"]))
 vEfficiencyRatio <- (datFishSuccessRate[,"Success"]/(datFishSuccessRate[,"Success"]+datFishSuccessRate[,"Fails"]))
 #vScoreIdx[is.nan(vScoreIdx) ] <- 0
 datFishSuccessRateMerged <- cbind(datFishSuccessRateMerged,vScoreIdx,vEfficiencyRatio)
@@ -60,13 +60,13 @@ densNLEffScore <- density(datFishSuccessRateActive[datFishSuccessRateActive$grou
 densLLEffScore <- density(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "LL",]$vEfficiencyRatio)
 
 dev.off() ##Clear Old plot
-plot(densLLEffScore,col=colourH[2],main="Hunt efficiency density",type="l",lwd=2,ylim=c(0,2.0))
+plot(densLLEffScore,col=colourH[2],main="Hunt efficiency density",type="l",lwd=2,ylim=c(0,2.0),xlim=c(0,1))
 lines(densNLEffScore,col=colourH[3],lwd=2)
 lines(densDLEffScore,col=colourH[1],lwd=2,xlab="Hunting efficiency score" )
+legend("topright",legend=paste(c("DL #","LL #","NL #"),c(densDLEffScore$n,densLLEffScore$n,densNLEffScore$n) ),fill = colourH)
 
 
 ## Plot Histograp of scor/Gaine ##
-
 ptbreaks <- seq(from=-1,to=1,by=2/10)
 layout(matrix(c(1,2,3), 3, 1 ,byrow=TRUE))
 hist(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "DL",]$vScoreIdx,col=colourR[1],main=paste("DL #",NROW(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "DL",]))
@@ -76,19 +76,49 @@ hist(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "LL",]$vScoreI
 hist(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "NL",]$vScoreIdx,col=colourR[3],main=paste("NL #",NROW(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "NL",]))
      ,xlab="Hunt efficiency score",breaks=ptbreaks,ylim=c(0,20))
 
-## Plot Density 
+## Plot Density of Hunting POWER S^2/(S+F)
 densDLScore <- density(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "DL",]$vScoreIdx)
 densNLScore <- density(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "NL",]$vScoreIdx)
 densLLScore <- density(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "LL",]$vScoreIdx)
 
-dev.off() ##Clear Old plot
-plot(densLLScore,col=colourH[2],main="Hunt score density",
-     xlab="Hunting score  " ,type="l",lwd=2,ylim=c(0,1.0))
-lines(densNLScore,col=colourH[3],lwd=2)
-lines(densDLScore,col=colourH[1],lwd=2)
+cdfDLScore <- ecdf(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "DL",]$vScoreIdx)
+cdfNLScore <- ecdf(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "NL",]$vScoreIdx)
+cdfLLScore <- ecdf(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "LL",]$vScoreIdx)
 
+dev.off() ##Clear Old plot
+ plot(densLLScore,col=colourH[2],main="Hunt Power",
+      xlab=expression(S^2/(S+F),paste("")) ,type="l",lwd=2,ylim=c(0,0.7))
+ lines(densNLScore,col=colourH[3],lwd=2)
+ lines(densDLScore,col=colourH[1],lwd=2)
+ legend("topright",legend=paste(c("DL #","LL #","NL #"),c(densDLScore$n,densLLScore$n,densNLScore$n) ),fill = colourH)
+
+## Plot CDF ##
+plot(cdfDLScore,lty=2,lwd=1,col=colourH[1],xlim=c(0,12),xlab=expression(S^2/(S+F),paste("")) )
+plot(cdfLLScore,add=T,lty=1,lwd=2,col=colourH[2])
+plot(cdfNLScore,add=T,lty=1,lwd=2,col=colourH[3])
+legend("bottomright",legend=paste(c("DL #","LL #","NL #"),c(densDLScore$n,densLLScore$n,densNLScore$n) ),fill = colourH)
+
+## Motivation
+densDLMotivation <- density(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "DL",]$Success+datFishSuccessRateActive[datFishSuccessRateActive$groupID == "DL",]$Fails)
+densNLMotivation <- density(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "NL",]$Success+datFishSuccessRateActive[datFishSuccessRateActive$groupID == "NL",]$Fails)
+densLLMotivation <- density(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "LL",]$Success+datFishSuccessRateActive[datFishSuccessRateActive$groupID == "LL",]$Fails)
+plot(densLLMotivation,col=colourH[2],main="Hunt score density",
+     xlab="Hunting score  " ,type="l",lwd=2,ylim=c(0,0.1))
+lines(densNLMotivation,col=colourH[3],lwd=2)
+lines(densDLMotivation,col=colourH[1],lwd=2)
+legend("topright",legend=paste(c("DL #","LL #","NL #"),c(densDLMotivation$n,densLLMotivation$n,densNLMotivation$n) ),fill = colourH)
+
+
+hist(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "DL",]$Success+datFishSuccessRateActive[datFishSuccessRateActive$groupID == "DL",]$Fails,breaks=30)
+hist(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "NL",]$Success+datFishSuccessRateActive[datFishSuccessRateActive$groupID == "NL",]$Fails,breaks=30)
+hist(datFishSuccessRateActive[datFishSuccessRateActive$groupID == "LL",]$Success+datFishSuccessRateActive[datFishSuccessRateActive$groupID == "LL",]$Fails,breaks=30)
 
 ##How Many Fish From Each Group Have A Score Higher Than :
 tblSuccessDist <- table(datFishSuccessRate[datFishSuccessRate$Success > 0, ]$groupID,datFishSuccessRate[datFishSuccessRate$Success > 0, ]$Success )
 
-tblSuccessDist
+
+table(datFishSuccessRateActive[datFishSuccessRateActive$vEfficiencyRatio > 0.0,]$groupID,
+      round(datFishSuccessRateActive[datFishSuccessRateActive$vEfficiencyRatio > 0.0,]$Success*datFishSuccessRateActive[datFishSuccessRateActive$vEfficiencyRatio > 0.0,]$vEfficiencyRatio*10)/10 )
+
+
+hist(round(datFishSuccessRateActive[datFishSuccessRateActive$vEfficiencyRatio > 0.0,]$Success*datFishSuccessRateActive[datFishSuccessRateActive$vEfficiencyRatio > 0.0,]$vEfficiencyRatio*10)/10)
