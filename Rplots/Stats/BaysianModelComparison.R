ca513c111794e9947fe8c8a5f4b0ccb2ab57c70f
kostas lagogiannis <costaslag@gmail.com>
2019-01-18 20:25:51 +0000

    Clarified BaysianModel Comparison.

--- Rplots/Stats/BaysianModelComparison.R
+++ Rplots/Stats/BaysianModelComparison.R
@@ -1,6 +1,7 @@
 ### Model Comparison ###
 
-### Model Evidence ### 
+### Model Evidence - Using a Baysian Factor #comparison## 
+## The Prior is  very imporrtant 
 ## Compare Likelyhoods between models for undershooting (linear slope fit) 
 ## Establish if DryFed Data Belong to NF rather then LF
 ## 
@@ -12,26 +13,6 @@ library(mvtnorm)
 
 ##Load Data (See stat_LinRegression_TurnVsBearing)
 
-### Plot Undershoot Raw Data
-hist(dataLL$turn/dataLL$bearing)
-hist(dataNL$turn/dataNL$bearing)
-hist(dataDL$turn/dataDL$bearing)
-
-##Synthetic - TEst Data
-x=seq(-65,65,3)
-c11=0.5
-c12=0.99
-c13=14 ##SD
-c21=0.5
-c22=0.60
-c23=14 ##SED
-
-test_data1=cbind(x,c11+c12*x+rnorm(length(x),sd=c13))
-test_data2=cbind(x,c21+c22*x+rnorm(length(x),sd=c23))
-test_data3=rbind(test_data1,test_data2)
-
-plot(test_data1,ylim=c(-70,70),xlim=c(-70,70) )
-points(test_data2,col="red")
 ###
 
 getParams <- function(data,a0=1,b0=1,Lambda0=lambda0){
@@ -60,8 +41,6 @@ DrawPostParams<- function(par){
 }
 
 genManyPar <- function(par,n=100){
-  plot(cbind(dataLL$turn,dataLL$bearing),ylim=c(-70,70),xlim=c(-70,70) )
-  
   x=seq(-70,70,.2)
   #plot(NA,xlim=range(x),ylim=range(x),type='n')
   for(i in 1:n){
@@ -70,19 +49,6 @@ genManyPar <- function(par,n=100){
   }
 }
 
-
-## Example ##
-p1=getParams(test_data1); lML1=logML(p1)
-p2=getParams(test_data2); lML2=logML(p2)
-p3=getParams(test_data3); lML3=logML(p3)
-## +ve means they are from separate Sources
-## -ve is interpreted as common model best describes these
-logR=(lML1+lML2)-lML3
-
-##On to the real Data - PRIORS Are very important 
-## in managing to separate te 2 cases where a common source exists and 
-## Priors plot(r,dinvgamma(r, 15, 1290))
-
 a0=15
 b0=1300
 sigma0 = 100;
@@ -90,23 +56,79 @@ sigma0 = 100;
 lambda0 = matrix(c(sigma0*10000,0,0,sigma0),2,2)
 
 
-MLparamsLL <- getParams( cbind(dataLL$turn,dataLL$bearing),a0,b0,lambda0 )
-MLparamsDL <- getParams( cbind(dataDL$turn,dataDL$bearing),a0,b0,lambda0 )
-MLparamsNL <- getParams( cbind(dataNL$turn,dataNL$bearing),a0,b0,lambda0 )
+##Synthetic - TEst Data
+x=seq(-65,65,3)
+c11=0.5
+c12=0.99
+c13=14 ##SD
+c21=0.5
+c22=0.60
+c23=14 ##SED
 
+test_data1=cbind(x,c11+c12*x+rnorm(length(x),sd=c13))
+test_data2=cbind(x,c21+c22*x+rnorm(length(x),sd=c23))
+test_data3=rbind(test_data1,test_data2)
+
+
+plot(test_data1,ylim=c(-70,70),xlim=c(-70,70) )
+points(test_data2,col="red")
+#plot(cbind(dataLL$turn,dataLL$bearing),ylim=c(-70,70),xlim=c(-70,70) )
+
+
+## Example ##
+p1=getParams(test_data1); lML1=logML(p1)
+p2=getParams(test_data2); lML2=logML(p2)
+p3=getParams(test_data3); lML3=logML(p3)
+
+
+genManyPar(p1)
+genManyPar(p2)
+genManyPar(p3)
+
+
+## +ve means they are from separate Sources
+## -ve is interpreted as common model best describes these
+logR=(lML1+lML2)-lML3
+
+##On to the rREAL Data - PRIORS Are very important 
+## in managing to separate te 2 cases where a common source exists and 
+## Priors plot(r,dinvgamma(r, 15, 1290))
+
+a0=15
+b0=1300
+sigma0 = 10;
+##This is inverse Variance
+#lambda0 = matrix(c(sigma0*10000,0,0,sigma0),2,2)
+lambda0 = matrix(c(sigma0*1,0,0,sigma0),2,2)
+
+
+##DATA Combinations ##
+datLL <- cbind(dataLL$turn,dataLL$bearing)
+datDL <- cbind(dataDL$turn,dataDL$bearing)
+datNL <- cbind(dataNL$turn,dataNL$bearing)
 dataNLDL <- rbind(cbind(dataNL$turn,dataNL$bearing),cbind(dataDL$turn,dataDL$bearing))
-MLparamsNLDL <- getParams( dataNLDL,a0,b0 )
-
 dataDLLL <- rbind(cbind(dataDL$turn,dataDL$bearing),cbind(dataLL$turn,dataLL$bearing))
-MLparamsDLLL <- getParams( dataDLLL,a0,b0 )
-
 dataLLNL <- rbind(cbind(dataNL$turn,dataNL$bearing),cbind(dataLL$turn,dataLL$bearing))
+
+MLparamsLL <- getParams( datLL,a0,b0,lambda0 )
+MLparamsDL <- getParams( datDL,a0,b0,lambda0 )
+MLparamsNL <- getParams( datNL,a0,b0,lambda0 )
+MLparamsNLDL <- getParams( dataNLDL,a0,b0 )
+MLparamsDLLL <- getParams( dataDLLL,a0,b0 )
 MLparamsLLNL <- getParams( dataLLNL,a0,b0 )
 
 ###Plot Lines
-plot(cbind(dataLL$turn,dataLL$bearing))
-points(cbind(dataNL$turn,dataNL$bearing),col="red")
-points(cbind(dataDL$turn,dataDL$bearing),col="blue")
+plot(datLL,col="green")
+par(col="green")
+genManyPar(MLparamsLL)
+
+points(datNL,col="red")
+par(col="red")
+genManyPar(MLparamsNL)
+points(datDL,col="blue")
+par(col="blue")
+genManyPar(MLparamsDL)
+
 
 ## Calcilate Probability of Model Given Data
 logML_LL <- logML(MLparamsLL)
@@ -116,6 +138,7 @@ logML_NLDL <- logML(MLparamsNLDL)
 logML_DLLL <- logML(MLparamsDLLL)
 logML_LLNL <- logML(MLparamsLLNL)
 
+
 ## Compare Models On Log Likehoods
 ## +ve means they are from separate Sources
 ## -ve is interpreted as common model best describes these
@@ -123,8 +146,17 @@ logR_DLNL=(logML_DL+logML_NL)-logML_NLDL
 logR_LLDL=(logML_DL+logML_LL)-logML_DLLL
 logR_LLNL=(logML_NL+logML_LL)-logML_LLNL
 
+message(paste("a0:",a0,"b0:",b0,"sigma0:",sigma0))
+message(paste(" LL Vs DL:", logR_LLDL,ifelse(logR_LLDL < 0," Common Model","Separate"),
+              "\n LL vs NL:",logR_LLNL,ifelse(logR_LLNL < 0," Common Model","Separate") ,
+              "\n DL vs NL",logR_DLNL, ifelse(logR_DLNL < 0," Common Model","Separate") )  )
 
-genManyPar(MLparamsLL)
+
+
+### Plot Undershoot Raw Data
+hist(dataLL$turn/dataLL$bearing)
+hist(dataNL$turn/dataNL$bearing)
+hist(dataDL$turn/dataDL$bearing)
 
 
 
@@ -160,7 +192,7 @@ getParams_old <- function(data,a0=1,b0=1,sigma0=1){
 ##Marginal Likelyhood 
 MarginalLikelihood <- function(MLParams,a0,b0)
 {
-  return (1/(2*pi)^(MLParams$n/2))* sqrt( det(diag(sigma0,2))/det( MLParams$lambda))*((b0^a0)/(MLParams$b^MLParams$a)) *(gamma(MLParams$a)/gamma(a
+  return (1/(2*pi)^(MLParams$n/2))* sqrt( det(diag(sigma0,2))/det( MLParams$lambda))*((b0^a0)/(MLParams$b^MLParams$a)) *(gamma(MLParams$a)/gamma(a))
 }
 
 
