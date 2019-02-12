064528a600b604ecde90cfba5ca2c46234be8642
kostas lagogiannis <costaslag@gmail.com>
2019-02-12 19:31:14 +0000

    Added Inffered Distribution of  Hunt Rate To Stat Plot, Fixed Colouring Order

--- Rplots/Stats/stat_HuntRateInPreyRange.R
+++ Rplots/Stats/stat_HuntRateInPreyRange.R
@@ -147,14 +147,14 @@ plotDurationDensityFitComparison <- function(datHDuration,drawDur,lcolour,HLim,n
 
 
 ## Compare Model TO Data Using CDF ##
-plotEventCountDistribution_cdf <- function(datHEventCount,drawHEvent,lcolour,lpch,lty,Plim,nplotSamples,newPlot = FALSE)
+plotEventCountDistribution_cdf <- function(datHEventCount,drawHEvent,lcolour,lpch,lty,Plim,nplotSamples=100,newPlot = FALSE)
 {
   XLim <- 30
   x <- seq(0,XLim,1)
 
   cdfD_N <- ecdf(datHEventCount[,2])
 
-  plot(cdfD_N(x),col=lcolour,pch=16,xlab=NA,ylab=NA,main="",xlim=c(0,XLim),ylim=c(0,1),cex=1.5,cex.lab=1.5,add=!newPlot)
+  plot(cdfD_N(x),col=lcolour,pch=21,xlab=NA,ylab=NA,main="",xlim=c(0,XLim),ylim=c(0,1),cex=1.5,cex.lab=1.5,add=!newPlot)
   ##Construct CDF of Model by Sampling randomly from Model distribution for exp rate parameter
   for (c in 1:NROW(drawHEvent$q[1,1,])) {
     for (q in  tail(drawHEvent$q[,,c],nplotSamples) )
@@ -163,12 +163,12 @@ plotEventCountDistribution_cdf <- function(datHEventCount,drawHEvent,lcolour,lpc
       lines(cumsum(cdfM),col=lcolour,lty=lty) #add=TRUE,
     }
   }
-  points(cdfD_N(x),col=colourH[4],pch=lpch,xlab=NA,ylab=NA,main="",xlim=c(0,XLim),ylim=c(0,1),cex=1.5,cex.lab=1.5,add=TRUE)
+  points(cdfD_N(x),col=colourH[4],pch=16,xlab=NA,ylab=NA,main="",xlim=c(0,XLim),ylim=c(0,1),cex=1.5,cex.lab=1.5,add=TRUE)
   #axis(side = 4)
   #mtext(side = 4, line = 2.1, 'Counts')
   ### Draw Distribution oF Hunt Rates - 
   ##(z= p/(1-p))
-  hist( (1-tail(drawHEvent$q[,,c],nplotSamples))/tail(drawHEvent$q[,,c],nplotSamples)  )
+#  hist( (1-tail(drawHEvent$q[,,c],nplotSamples))/tail(drawHEvent$q[,,c],nplotSamples)  )
 }
 
 
@@ -231,18 +231,18 @@ datHuntStat <- makeHuntStat(datHuntLabelledEventsSBMerged_filtered)
 ## Added Larva ID to Check for Correlation Through Time of Day - Surrogate as LarvaID;s increased through the day of the experiment from 1-4
 datHuntVsPreyLL <- cbind(datHuntStat[,"vHInitialPreyCount"]$LL , as.numeric(datHuntStat[,"vHLarvaEventCount"]$LL),as.numeric(datHuntStat[,"vHDurationPerLarva"]$LL ),datHuntStat[,"vIDLookupTable"]$LL$larvaID )
 datHuntVsPreyLE <- cbind(datHuntStat[,"vHInitialPreyCount"]$LE , as.numeric(datHuntStat[,"vHLarvaEventCount"]$LE),as.numeric(datHuntStat[,"vHDurationPerLarva"]$LE ),datHuntStat[,"vIDLookupTable"]$LE$larvaID  )
-datHuntVsPreyL <- datHuntVsPreyLE#rbind(datHuntVsPreyLL,datHuntVsPreyLE)
+datHuntVsPreyL <- datHuntVsPreyLL#rbind(datHuntVsPreyLL,datHuntVsPreyLE)
 datHuntVsPreyL <- datHuntVsPreyL[!is.na(datHuntVsPreyL[,1]) & datHuntVsPreyL[,2] < 25,]
 
 
 datHuntVsPreyNL <- cbind(datHuntStat[,"vHInitialPreyCount"]$NL , as.numeric(datHuntStat[,"vHLarvaEventCount"]$NL),as.numeric(datHuntStat[,"vHDurationPerLarva"]$NL),datHuntStat[,"vIDLookupTable"]$NL$larvaID )
 datHuntVsPreyNE <- cbind(datHuntStat[,"vHInitialPreyCount"]$NE , as.numeric(datHuntStat[,"vHLarvaEventCount"]$NE),as.numeric(datHuntStat[,"vHDurationPerLarva"]$NE),datHuntStat[,"vIDLookupTable"]$NE$larvaID  )
-datHuntVsPreyN <- datHuntVsPreyNE #rbind(datHuntVsPreyNL,datHuntVsPreyNE)
+datHuntVsPreyN <- datHuntVsPreyNL #rbind(datHuntVsPreyNL,datHuntVsPreyNE)
 datHuntVsPreyN <- datHuntVsPreyN[!is.na(datHuntVsPreyN[,1]) & datHuntVsPreyN[,2] < 25,]
 
 datHuntVsPreyDL <- cbind(datHuntStat[,"vHInitialPreyCount"]$DL , as.numeric(datHuntStat[,"vHLarvaEventCount"]$DL),as.numeric(datHuntStat[,"vHDurationPerLarva"]$DL ),datHuntStat[,"vIDLookupTable"]$DL$larvaID  )
 datHuntVsPreyDE <- cbind(datHuntStat[,"vHInitialPreyCount"]$DE , as.numeric(datHuntStat[,"vHLarvaEventCount"]$DE),as.numeric(datHuntStat[,"vHDurationPerLarva"]$DE ),datHuntStat[,"vIDLookupTable"]$DE$larvaID  )
-datHuntVsPreyD <-datHuntVsPreyDE #rbind(datHuntVsPreyDL,datHuntVsPreyDE)
+datHuntVsPreyD <-datHuntVsPreyDL #rbind(datHuntVsPreyDL,datHuntVsPreyDE)
 datHuntVsPreyD <- datHuntVsPreyD[!is.na(datHuntVsPreyD[,1]) & datHuntVsPreyD[,2] < 25,] ##Remove NA And High Fliers
 
 ## Check Number of Hunt Events For A LarvaID 
@@ -321,7 +321,7 @@ x <- seq(0,Plim,0.1)
 ##Show Alignment with Empirical Distribution of HuntEvent Numbers
 ## Number of Hunt Events Per Larva
 plotsamples <- 70
-layout(matrix(c(1,2,3,4,5,5), 3,2, byrow = FALSE))
+layout(matrix(c(1,2,3,4,5,6), 3,2, byrow = FALSE))
 ##Margin: (Bottom,Left,Top,Right )
 par(mar = c(3.9,3.01,1,1))
 lineTypeL[1] <- 1
@@ -329,11 +329,11 @@ pchL[1] <- 21
 pchL[2] <- 21
 pchL[3] <- 21
 plotEventCountDistribution_cdf(datHuntVsPreyN,drawNL2,colourH[1],pchL[1],lineTypeL[1],Plim,plotsamples,newPlot=TRUE )
-legend("bottomright",legend = c(paste("Empirical NF #",nDatNF ),paste("Model Sampled ")), col=colourH[1], pch=c(pchL[1],NA),lty=c(NA,1),lwd=2,cex=1.1,bg="white" )
-plotEventCountDistribution_cdf(datHuntVsPreyL,drawLL2,colourH[2],pchL[2],lineTypeL[2],Plim,plotsamples,newPlot=TRUE )
-legend("bottomright",legend = c(paste("Empirical LF #",nDatLF ),paste("Model Sampled ")), col=colourH[2], pch=c(pchL[2],NA),lty=c(NA,1),lwd=2,cex=1.1,bg="white" )
-plotEventCountDistribution_cdf(datHuntVsPreyD,drawDL2,colourH[3],pchL[3],lineTypeL[3],Plim,plotsamples,newPlot=TRUE  )
-legend("bottomright",legend = c(paste("Empirical DF #",nDatDF ),paste("Model Sampled ")), col=colourH[3], pch=c(pchL[3],NA),lty=c(NA,1),lwd=2,cex=1.1,bg="white" )
+legend("bottomright",legend = c(paste("Empirical NF #",nDatNF ),paste("Model Sampled ")), col=colourL[1], pch=c(pchL[1],NA),lty=c(NA,1),lwd=2,cex=1.1,bg="white" )
+plotEventCountDistribution_cdf(datHuntVsPreyL,drawLL2,colourH[2],pchL[2],lineTypeL[1],Plim,plotsamples,newPlot=TRUE )
+legend("bottomright",legend = c(paste("Empirical LF #",nDatLF ),paste("Model Sampled ")), col=colourL[2], pch=c(pchL[2],NA),lty=c(NA,1),lwd=2,cex=1.1,bg="white" )
+plotEventCountDistribution_cdf(datHuntVsPreyD,drawDL2,colourH[3],pchL[3],lineTypeL[1],Plim,plotsamples,newPlot=TRUE  )
+legend("bottomright",legend = c(paste("Empirical DF #",nDatDF ),paste("Model Sampled ")), col=colourL[3], pch=c(pchL[3],NA),lty=c(NA,1),lwd=2,cex=1.1,bg="white" )
 mtext(side = 1,cex=0.8, line = 2.2, "Hunt Event Counts (N)")
 mtext(side = 2,cex=0.8, line = 2.2, " F(x < N) ")
 
@@ -343,16 +343,30 @@ Ylim <- 50
 pBW <- 0.001
 densHEventSampled_L <-density(drawLL2$q[1,,schain],bw=pBW);densHEventSampled_D <-density(drawDL2$q[1,,schain],bw=pBW);densHEventSampled_N <-density(drawNL2$q[1,,schain],bw=pBW)   
 
-plot(densHEventSampled_N$x, densHEventSampled_N$y,type='l',xlim=c(0,0.4),ylim=c(0,Ylim),lty=lineTypeL[1],col=colourL[3],lwd=4,ylab=NA,xlab=NA)
+plot(densHEventSampled_N$x, densHEventSampled_N$y,type='l',xlim=c(0,0.4),ylim=c(0,Ylim),lty=lineTypeL[1],col=colourL[1],lwd=4,ylab=NA,xlab=NA)
 lines(densHEventSampled_L$x, densHEventSampled_L$y,type='l',xlim=c(0,0.4),ylim=c(0,Ylim),lty=lineTypeL[2],col=colourL[2],lwd=4,ylab=NA,xlab=NA)
-lines(densHEventSampled_D$x, densHEventSampled_D$y,type='l',xlim=c(0,0.4),ylim=c(0,Ylim),lty=lineTypeL[3],col=colourL[1],lwd=4,ylab=NA,xlab=NA)
-mtext(side = 1,cex=0.8, line =2.2, expression(paste("Rate Parameter (",lambda,") ") ) )
+lines(densHEventSampled_D$x, densHEventSampled_D$y,type='l',xlim=c(0,0.4),ylim=c(0,Ylim),lty=lineTypeL[3],col=colourL[3],lwd=4,ylab=NA,xlab=NA)
+mtext(side = 1,cex=0.8, line =2.2, expression(paste("Model  Parameter (p",") ") ) )
 mtext(side = 2,cex=0.8, line =2.2, paste("Density Bw:",densHEventSampled_L$bw))
 legend("topright",legend = c(paste("NF " ),paste("LF ") ,paste("DF ") ) ,
-       col=colourH,lty=lineTypeL,lwd=3,seg.len=3,cex=1.1)
+       col=colourL,lty=lineTypeL,lwd=3,seg.len=3,cex=1.1)
+
+### Draw Distribution oF Hunt Rates - 
+##(z= p/(1-p))
+densHEventHuntRate_LE <-density((1-tail(drawLL2$q[,,schain],nplotSamples))/tail(drawLL2$q[,,schain],nplotSamples));
+densHEventHuntRate_DE <- density((1-tail(drawDL2$q[,,schain],nplotSamples))/tail(drawDL2$q[,,schain],nplotSamples));   
+densHEventHuntRate_NE <- density((1-tail(drawNL2$q[,,schain],nplotSamples))/tail(drawNL2$q[,,schain],nplotSamples));   
+
+plot(densHEventHuntRate_LE$x, densHEventHuntRate_LE$y,type='l',xlim=c(0,12),ylim=c(0,1),lty=lineTypeL[2],col=colourL[2],lwd=4,ylab=NA,xlab=NA)
+lines(densHEventHuntRate_NE$x,densHEventHuntRate_NE$y,type='l',xlim=c(0,12),ylim=c(0,1),lty=lineTypeL[1],col=colourL[1],lwd=4,ylab=NA,xlab=NA)
+lines(densHEventHuntRate_DE$x, densHEventHuntRate_DE$y,type='l',xlim=c(0,12),ylim=c(0,1),lty=lineTypeL[3],col=colourL[3],lwd=4,ylab=NA,xlab=NA)
+legend("topright",legend = c(paste("NF " ),paste("LF ") ,paste("DF ") ) ,
+       col=colourL,lty=lineTypeL,lwd=3,seg.len=3,cex=1.1)
+mtext(side = 1,cex=0.8, line =2.2, expression(paste(" Hunt Rate  (",lambda,") ") ) )
+
 ##BoxPlot
 boxplot(datHuntVsPreyN[,2],datHuntVsPreyL[,2] ,datHuntVsPreyD[,2],
-        main=NA,notch=TRUE,col=colourH,names=c("NF","LF","DF"),ylim=c(0,60)  )
+        main=NA,notch=TRUE,col=colourH,names=c("NF","LF","DF"),ylim=c(0,50)  )
 mtext(side = 2,cex=0.8, line =2.2, "Hunt Event Counts (N)")
 
 dev.off() 
--- Rplots/main_TrackingAnalysis.r
+++ Rplots/main_TrackingAnalysis.r
@@ -42,11 +42,12 @@ rfc <- colorRampPalette(rev(brewer.pal(8,'Spectral')));
 r <- c(rfc(7),"#FF0000");
 
 ##For the 3 Groups 
-colourH <- c(rgb(0.01,0.01,0.9,0.1),rgb(0.01,0.7,0.01,0.1),rgb(0.9,0.01,0.01,0.1),rgb(0.00,0.00,0.0,1.0)) ##Legend
-colourP <- c(rgb(0.01,0.01,0.8,0.5),rgb(0.01,0.6,0.01,0.5),rgb(0.8,0.01,0.01,0.5),rgb(0.00,0.00,0.0,1.0)) ##points]
-colourR <- c(rgb(0.01,0.01,0.9,0.4),rgb(0.01,0.7,0.01,0.4),rgb(0.9,0.01,0.01,0.4),rgb(0.00,0.00,0.0,1.0)) ##Region (Transparency)
-colourD <- c("#0303E6AA","#03B303FF","#E60303AA")
-colourL <- c("#0303E6AF","#03B303AF","#E60303AF")
+###  NF, LF, DF , Black Colouring 
+colourH <- c(rgb(0.9,0.01,0.01,0.1),rgb(0.01,0.7,0.01,0.1),rgb(0.01,0.01,0.9,0.1),rgb(0.00,0.00,0.0,1.0)) ##Legend
+colourP <- c(rgb(0.8,0.01,0.01,0.5),rgb(0.01,0.6,0.01,0.5),rgb(0.01,0.01,0.8,0.5),rgb(0.00,0.00,0.0,1.0)) ##points]
+colourR <- c(rgb(0.9,0.01,0.01,0.4),rgb(0.01,0.7,0.01,0.4),rgb(0.01,0.01,0.9,0.4),rgb(0.00,0.00,0.0,1.0)) ##Region (Transparency)
+colourD <- c("#E60303AA","#03B303FF","#0303E6AA")
+colourL <-c("#E60303AF","#03B303AF","#0303E6AF")
 
 pchL <- c(16,17,4) ## The style of bullet used for each group DL, LL, NL
 lineTypeL <- c(2,1,3) ## The style of bullet used for each group DL, LL, NL
