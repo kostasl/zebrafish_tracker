879457c5c4a5f393db07359e6c34e475af0b725b
Kostas Lago <costaslag@gmail.com>
2019-05-22 16:58:14 +0100

    Adjust priors

--- Rplots/Stats/stat_CaptureSpeedVsDistanceToPrey.R
+++ Rplots/Stats/stat_CaptureSpeedVsDistanceToPrey.R
@@ -59,7 +59,7 @@ for  (g in 1:2)
 
   ## High speed Capture Cluster
   mu[2,1] ~ dnorm(0.5,0.01) ##Distance prey
-  mu[2,2] ~ dnorm(30,0.01) ##cap speed
+  mu[2,2] ~ dnorm(30,0.001) ##cap speed
   sigma[2,2] ~ dunif(0,5) ##the cap speed sigma 
 
 ## Synthesize data from the distribution
