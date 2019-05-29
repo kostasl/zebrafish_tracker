e5bd1548fb90b2c6a49b1343346aead7b70879f8
Kostas Lago <costaslag@gmail.com>
2019-05-29 15:33:39 +0100

    added init lists for model

--- Rplots/Stats/stat_CaptureSpeedVsUndershoot.r
+++ Rplots/Stats/stat_CaptureSpeedVsUndershoot.r
@@ -80,11 +80,11 @@ initfunct <- function(nchains,N)
   initlist <- replicate(nchains,list(mID=c(rbinom(N,1,0.5)), ##Base Line Vergence Prior to HuntOn
                                      sigma = matrix(c (  c(runif(1,min=0,max=0.1),runif(1,min=0,max=2)),
                                                          c(runif(1,min=0,max=0.1),runif(1,min=0,max=15))  ),nrow=2,byrow=T  ),
-                                     mu  = matrix(c (  c( rnorm(1,mean=1,sd=sqrt(1/10) ), rnorm(1,mean=8,sd=sqrt(1/2) ) ),
-                                                        c( rnorm(1,mean=1, sd=sqrt(1/10) ) , rnorm(1,mean=30, sd=sqrt(1/0.1) )    ) )
-                                                     ,nrow=2,byrow = T  ),
+#                                     mu  = matrix(c (  c( rnorm(1,mean=1,sd=sqrt(1/10) ), rnorm(1,mean=8,sd=sqrt(1/2) ) ),
+#                                                        c( rnorm(1,mean=1, sd=sqrt(1/10) ) , rnorm(1,mean=30, sd=sqrt(1/0.1) )    ) )
+#                                                     ,nrow=2,byrow = T  ),
                                      ".RNG.name"="base::Super-Duper",
-                                     ".RNG.seed"=1),
+                                     ".RNG.seed"=round(runif(1,0,20000)) ),
                                      simplify=FALSE)
   return(initlist)
 }
@@ -153,13 +153,13 @@ for  (g in 1:2)
 }
 ## Low Speed Captcha cluster
 
-mu[1,1] ~ dnorm(1,0.000001)T(0,) ##undershoot 
+mu[1,1] ~ dnorm(1,0.000001)T(0,2) ##undershoot 
 mu[1,2] ~ dnorm(8,0.1)T(0,) ##cap speed
 sigma[1,2] ~ dunif(0,2) ##the low cap speed sigma 
 sigma[1,1] ~ dunif(0,0.1) ##undershoot prey - Keep it broader within the expected limits 
 
 ## High speed Capture Cluster
-mu[2,1] ~ dnorm(1,0.000001)T(0,) ##undershoot
+mu[2,1] ~ dnorm(1,0.000001)T(0,2) ##undershoot
 mu[2,2] ~ dnorm(30,0.0001)T(0,) ##cap speed
 sigma[2,2] ~ dunif(0,15) ##the cap speed sigma 
 sigma[2,1] ~ dunif(0,0.1) ##undershoot prey - Keep it narrow within the expected limits 
@@ -255,6 +255,12 @@ dDLb_rho<-density(tail(draw_DF$rho[1,,1],ntail),kernel="gaussian",bw=pBw)
 #dALLb_rho<-density(tail(draw_ALL$rho[,,1],ntail),kernel="gaussian",bw=pBw)
 
 
+###Check COnv
+
+plot(draw_LF$mu[1,1,,1],type='l',ylim=c(0,2),col=rfc(nchains)[1] )
+lines(draw_LF$mu[1,1,,2],type='l',ylim=c(0,2),col=rfc(nchains)[2] )
+lines(draw_LF$mu[1,1,,3],type='l',ylim=c(0,2),col=rfc(nchains)[3] )
+lines(draw_LF$mu[1,1,,5],type='l',ylim=c(0,2),col=rfc(nchains)[4] )
 ##Get the synthesized data:
 
 #plot(tail(draw_NF$x_rand[1,,1],ntail ),tail(draw_NF$x_rand[2,,1],ntail ),col=colourH[1])
