fff2deab13d8ea78bc6ca2c7295885e82bf1b2b4
kostasl <costaslag@gmail.com>
2019-08-26 18:35:41 +0100

    making the inf joint di

--- Rplots/Stats/stat_InformationTheory_TurnRatio.R
+++ Rplots/Stats/stat_InformationTheory_TurnRatio.R
@@ -21,3 +21,58 @@ datCapture_DL <- data.frame( cbind(DistanceToPrey=lFirstBoutPoints$DL[,"Distance
 datCapture_NL <- datCapture_NL[datCapture_NL$Validated == 1, ]
 datCapture_LL <- datCapture_LL[datCapture_LL$Validated == 1, ]
 datCapture_DL <- datCapture_DL[datCapture_DL$Validated == 1, ]
+
+
+
+
+
+##
+## Calc Info In single Sample & Hunt Event
+#InfoCalc <- function(DistRange,minDist,maxDist,Ulist)
+  
+  DistRange  <- seq(0,1,0.1)
+  SpeedRange <- seq(0,100,1) ##We limit The information Obtained To Reasonable Ranges Of Phi (Vergence Angle)
+  
+  
+  #Generates a list with all possible pairs of in the range of Phi And Distance X 
+  Grid <- expand.grid(SpeedRange,DistRange)
+  
+  ### Tally 2D data points in the grid
+  
+  
+  ## Normalize Grid To Sum of Points to obtain Joint Prob P(Capt Speed| Distance)
+  
+ 
+  PVec=rep(0,NROW(Grid)) 
+  ##Calc Density for all input Space
+  for (i in 1:NROW(Grid) )
+  {
+    PVec[i] <- phiDens(Grid[i,1],Grid[i,2],Ulist)
+  }
+  ##Normalize to Probability
+  PVec=PVec/sum(PVec)
+  
+  # Convert Pvec to a matrix / 
+  PMatrix=matrix(PVec,nrow=length(PhiRange),ncol=length(DistRange),byrow = FALSE)
+  
+  ##Image shows a transpose Of the PMatrix, - In reality Rows contain Phi, and Cols are X 
+  #image(t(PMatrix),y=PhiRange,x=DistRange)
+  MargVec=rowSums(PMatrix) ### Marginalize across X to obtain P(Response/Phi)
+  
+  Iloc=PMatrix/MargVec*length(DistRange) ##Information On Local x/For Each X - Assume X is unif. and so Prob[X]=1/Length(X)
+  
+  ###row sum
+  sel=PMatrix>0
+  #INFO=sums(PMatrix[sel]*log2(Iloc[sel]) )
+  ### Return Marginals I_xPhi
+  mInfo <- PMatrix*log2(Iloc)
+  
+  ##Calc Marginals only on relevant Region - up to min distance from Target
+  ##Make Binary Array indicating OutOf Bound regions
+  vIntRegion<-as.numeric( DistRange >= minDist)
+  mIntRegion <- t(matrix(vIntRegion,dim(mInfo)[2],dim(mInfo)[1]))
+  
+  INFO=colSums(mInfo*mIntRegion,na.rm=TRUE )
+  
+#  return(INFO)
+#}
