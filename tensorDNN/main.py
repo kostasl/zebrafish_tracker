0e31819344fc6ef4974c665b0fcd4bea7a72a2fe
Kostas Lagogiannis <costaslag@gmail.com>
2021-10-11 14:25:24 +0100

    Making tensorflow script

--- tensorDNN/main.py
+++ tensorDNN/main.py
@@ -1,9 +1,9 @@
 # This is a sample Python script.
-
-
+import matplotlib.pyplot
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import io
 import PIL
 import pathlib
 
@@ -28,157 +28,221 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 
+print(tf.version.VERSION)
+
 
 data_dir = pathlib.Path("/home/kostasl/workspace/zebrafishtrack/tensorDNN/trainset/")
 
 fish = list(data_dir.glob('./fish/*.jpg'))
-PIL.Image.open(str(fish[0]))
+#PIL.Image.open(str(fish[0]))
+
 
 nonfish = list(data_dir.glob('./nonfish/*.jpg'))
-PIL.Image.open(str(nonfish[0]))
+#PIL.Image.open(str(nonfish[0]))
 
 batch_size = 32
 img_height = 28
 img_width = 38
+def train_model(batch_size,img_height,img_width):
 
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  str(data_dir),
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  color_mode='grayscale',
-  batch_size=batch_size)
-
-
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  str(data_dir),
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  color_mode='grayscale',
-  batch_size=batch_size)
-
-class_names = train_ds.class_names
-print(class_names)
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+      str(data_dir),
+      validation_split=0.2,
+      subset="training",
+      seed=123,
+      image_size=(img_height, img_width),
+      color_mode='grayscale',
+      batch_size=batch_size)
 
 
-import matplotlib.pyplot as plt
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+      str(data_dir),
+      validation_split=0.2,
+      subset="validation",
+      seed=123,
+      image_size=(img_height, img_width),
+      color_mode='grayscale',
+      batch_size=batch_size)
 
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
-
-for image_batch, labels_batch in train_ds:
-  print(image_batch.shape)
-  print(labels_batch.shape)
-  break
-
-## Data Augmentation
-data_augmentation = keras.Sequential(
-  [
-    layers.experimental.preprocessing.RandomFlip("horizontal",
-                      input_shape=(img_height,  img_width, 1)),
-    layers.experimental.preprocessing.RandomRotation(0.05),
-    layers.experimental.preprocessing.RandomZoom(0.1),
-  ]
-)
-
-plt.figure(figsize=(10, 10))
-for images, _ in train_ds.take(1):
-  for i in range(9):
-    augmented_images = data_augmentation(images)
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(augmented_images[0].numpy().astype("uint8"))
-    plt.axis("off")
+    class_names = train_ds.class_names
+    print(class_names)
 
 
-## Config Dataset performance
-AUTOTUNE = tf.data.AUTOTUNE
+    plt.figure(figsize=(10, 10))
+    for images, labels in train_ds.take(1):
+      for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
 
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    for image_batch, labels_batch in train_ds:
+      print(image_batch.shape)
+      print(labels_batch.shape)
+      break
 
-##Standardize the data
-normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+    ## Data Augmentation
+    data_augmentation = keras.Sequential(
+      [
+        layers.experimental.preprocessing.RandomFlip("horizontal",
+                          input_shape=(img_height,  img_width, 1)),
+        layers.experimental.preprocessing.RandomRotation(0.05),
+        layers.experimental.preprocessing.RandomZoom(0.1),
+      ]
+    )
 
-## Create the model ##
-num_classes = 2
-
-model = Sequential([
-  data_augmentation,
-  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 1)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
-])
-##COMPILE MODEL
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+    plt.figure(figsize=(10, 10))
+    for images, _ in train_ds.take(1):
+      for i in range(9):
+        augmented_images = data_augmentation(images)
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(augmented_images[0].numpy().astype("uint8"))
+        plt.axis("off")
 
 
+    ## Config Dataset performance
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    ##Standardize the data
+    normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+
+    ## Create the model ##
+    num_classes = 2
+
+    model = Sequential([
+      data_augmentation,
+      layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 1)),
+      layers.Conv2D(16, 3, padding='same', activation='relu'),
+      layers.MaxPooling2D(),
+      layers.Conv2D(32, 3, padding='same', activation='relu'),
+      layers.MaxPooling2D(),
+      layers.Conv2D(64, 3, padding='same', activation='relu'),
+      layers.MaxPooling2D(),
+      layers.Flatten(),
+      layers.Dense(128, activation='relu'),
+      layers.Dense(num_classes)
+    ])
+    ##COMPILE MODEL
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+
+    model.summary()
+
+    checkpoint_path = "training_1/cp.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+
+    # Create a callback that saves the model's weights
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                     save_weights_only=True,
+                                                     verbose=1)
+
+
+    ## TRAIN MODEL
+    epochs=10
+    history = model.fit(
+      train_ds,
+      validation_data=val_ds,
+      epochs=epochs,
+      callbacks=[cp_callback]
+    )
+
+    ## Save Model ##
+    model.save('savedmodels/fishNet')
+
+
+    ## VISUALIZE RESULTS
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+
+
+## LOAD MODEL ##
+model = tf.keras.models.load_model('savedmodels/fishNet')
 model.summary()
 
-checkpoint_path = "training_1/cp.ckpt"
-checkpoint_dir = os.path.dirname(checkpoint_path)
 
-# Create a callback that saves the model's weights
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                 save_weights_only=True,
-                                                 verbose=1)
+## SLIDING WINDOW CLASSIFIER ##
+from tensorflow.keras.preprocessing import image
 
+# change this as you see fit
+image_path = '/home/kostasl/workspace/zebrafishtrack/tensorDNN/img_target/00240.png'
 
-## TRAIN MODEL
-epochs=10
-history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs,
-  callbacks=[cp_callback]
-)
-
-## Save Model ##
-model.save('savedmodels/fishNet')
+# Convert image to np.array
+imgScene = image.load_img(image_path,color_mode = "grayscale")
 
 
-## VISUALIZE RESULTS
+#plt.figure(figsize=(6, 6))
+#plt.imshow(image)
+#plt.show()
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+#most deep learning models expect a batch of images as input
+image_array = image.img_to_array(imgScene)
+img_batch = np.expand_dims(image_array,axis=0)
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+imgb_size = np.shape(img_batch)
+print(imgb_size)
 
-epochs_range = range(epochs)
+#img_preprocessed = tf.keras.preprocessing.preprocess_input(img_batch)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+# Sliding window
+y_len,x_len,_ = image_array.shape
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
+scale_x = 38
+scale_y = 28
 
-##VISUALIZE TRAINING RESULTS
 
+print("image size (%sx%s)",(y_len,x_len))
+probability_model = Sequential([model,
+                                layers.Softmax()])
+
+
+for y in range(0,y_len-scale_y,scale_y):
+    for x in range(0,x_len-scale_x,scale_x):
+        y_idxS = round(y)
+        y_idxT = round(y+scale_y)
+        x_idxS = round(x)
+        x_idxT = round(x+scale_x)
+        print('(%s,%s) -> (%s:%s),(%s:%s)' % (x + 1, y + 1,x_idxS,x_idxT,y_idxS,y_idxT))
+        cropped_image = img_batch[:,y_idxS:y_idxT,
+                                      x_idxS:x_idxT,:]
+        #imgByteArray = io.BytesIO()
+        #cropped_image.save(imgByteArray, format='JPEG')
+        #imgByteArray = imgByteArray.getvalue()
+        cimg_size = np.shape(cropped_image)
+        print(cimg_size)
+
+        # Classify
+        #classifier(imgByteArray,label_path,retrained_path)
+        predictions = probability_model.predict_classes(cropped_image)
+        print(predictions[0])
+        print("~~~ Predicted Class:")
+        pclass = np.argmax(predictions[0])
+        image.save_img("/home/kostasl/workspace/zebrafishtrack/tensorDNN/trainset/valid/fish/"+str(x)+"x"+str(y)+".jpg",cropped_image[0])
 
 
 ## END
