66ff85e3363b72595a8d1a6afa6de6547116ccd3
kostas lagogiannis <costaslag@gmail.com>
2022-09-29 23:02:12 +0100

    added specificity calc via frame counting

--- Rplots/DataLabelling/validateHunteventAgainstUserAnnotated.r
+++ Rplots/DataLabelling/validateHunteventAgainstUserAnnotated.r
@@ -7,7 +7,7 @@ setEnvFileLocations("LAPTOP") #HOME,OFFICE,#LAPTOP
 
 load("/media/kostasl/zFish-Heta-T7/OliviaExp/Appetitesamples/tracked_org/Analysis/dat//setn1_Dataset_VAL.RData")
 
-vExpID <- c(3)
+vExpID <- c(108)
 
 
 expID <- vExpID[1]
@@ -19,6 +19,8 @@ datHuntEventsM <- read.csv(
 ## Load Automated detection
 datHuntEvents <- detectHuntEvents(datAllFrames,vExpID,"LR",1)
 
+datExpFrames = datAllFrames[datAllFrames$expID == vExpID[1] ,]
+datExpEyeV = (datExpFrames$LEyeAngle-datExpFrames$REyeAngle)
 ## Make Unique pairs for each element of vector of start frames
 datAllStartFramePairs <- expand.grid(manual=datHuntEventsM$startFrame,automatic=datHuntEvents$startFrame)
 
@@ -35,8 +37,8 @@ vFrameDistToAutoDetectedHuntEvent <- tapply(datAllStartFramePairs_top$frameDista
 ## Get Number of Detected events - use thres between auto detected and manual event 
 vTruePositiveDetected <- vFrameDistToAutoDetectedHuntEvent[vFrameDistToAutoDetectedHuntEvent < HUNTEVENT_MATCHING_OFFSET] 
 nTruePositiveDetected <- NROW(vTruePositiveDetected)
-## The remaining manually labelled events that were not matched are counted as falselly classified as negative
-nFalseNegativeDetected <- NROW(vFrameDistToAutoDetectedHuntEvent) - NROW(vTruePositiveDetected)
+## The remaining manually labelled events that were not matched are counted as falsely classified as negative
+nFalseNegativeDetected <- NROW(datHuntEventsM) - NROW(vTruePositiveDetected)
 vValidatedAutoDetectedEvents <- as.numeric(names(vTruePositiveDetected))
 ## How likely is it that tracker detects a hunt event 
 sensitivity <- nTruePositiveDetected/(nTruePositiveDetected + nFalseNegativeDetected)
@@ -44,13 +46,19 @@ sensitivity <- nTruePositiveDetected/(nTruePositiveDetected + nFalseNegativeDete
 nFalsePositives <- NROW(datHuntEvents) - NROW(datHuntEventsM)
 ## Since we are classifying each frame, then here All non-Hunt Frames classified as such are True negatives - problem is the majority of frames are  true negatives are hunt events are generally rare
 # Sum Total Automatic Detected Hunt Frames
-nDetectedHuntFrames <- sum(datHuntEvents$endFrame - datHuntEvents$startFrame)  
-nTrueNegative <- 
+vValidatedAutoHuntEvents <- datAllStartFramePairs_top[datAllStartFramePairs_top$manual %in% as.numeric(names(vTruePositiveDetected)),"automatic"]
+# Count Number of Automatically Correctly Classified Frames
+nTruePositiveHuntFrames <- sum(datHuntEvents[datHuntEvents$startFrame %in% vValidatedAutoHuntEvents,]$endFrame - 
+                             datHuntEvents[datHuntEvents$startFrame %in% vValidatedAutoHuntEvents,]$startFrame )
+nFalsePositiveFrames <- sum(datHuntEvents[!datHuntEvents$startFrame %in% vValidatedAutoHuntEvents,]$endFrame - 
+                             datHuntEvents[!datHuntEvents$startFrame %in% vValidatedAutoHuntEvents,]$startFrame )
+# Count Total Frames With EyesVerged>THRESHOLD which have been correctly classified as non-Hunting 
+nTrueNegative <- NROW(datExpEyeV[datExpEyeV > G_THRESHUNTVERGENCEANGLE])-nTruePositiveHuntFrames
+##  Specificity 
+specificity <- nTrueNegative/(nTrueNegative+nFalsePositiveFrames)
 ## Plot Manual and Automatic
 
-datExpFrames = datAllFrames[datAllFrames$expID == vExpID[1] ,]
-
-plot(datExpFrames$frameN,(datExpFrames$LEyeAngle-datExpFrames$REyeAngle),type="l",ylim=c(0,70),ylab="Eye vergence",xlab="frame N",xlim=c(0,10000))
+plot(datExpFrames$frameN,datExpEyeV,type="l",ylim=c(0,70),ylab="Eye vergence",xlab="frame N")
 abline(h=G_THRESHUNTVERGENCEANGLE,lwd=2,lty=2)
 points(datAllStartFramePairs_top$automatic,rep(60,NROW(datAllStartFramePairs_top)),pch=2,col="red")
 #points(datAllStartFramePairs_top$manual,rep(64,NROW(datAllStartFramePairs_top)),pch=6,col="blue")
@@ -58,5 +66,5 @@ points(datHuntEventsM$startFrame,rep(63,NROW(datHuntEventsM)),pch=25,col="blue")
 points(vValidatedAutoDetectedEvents,rep(64,NROW(vValidatedAutoDetectedEvents)),pch=25,col="purple")
 legend("bottomright",
        legend = c(paste("auto n",NROW(datHuntEvents)),paste("manual n",NROW(datHuntEventsM)),paste("matched n",nTruePositiveDetected) ) ,col=c("red","blue","purple"),pch=c(2,25,25) )
-title(paste("F", unique(datHuntEvents$expID) ,"Hunt event detection sensitivity:",prettyNum(sensitivity*100,digits=4)) )
+title(paste("F", unique(datHuntEvents$expID) ,"Hunt event sensitivity:",prettyNum(sensitivity*100,digits=4)," specificity:",prettyNum(specificity*100,digits=4) ) )
 
