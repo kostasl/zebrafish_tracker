c14ae8cf8c423315a332e20d06e98cde68e63ede
kostas lagogiannis <costaslag@gmail.com>
2023-01-09 19:07:39 +0000

    moved Analysis scripts to dedicated repo

--- Rplots/fixTrackerFiles.sh
+++
@@ -1,5 +0,0 @@
-
-
-grep -rl   '		 templateScore' *.csv | xargs sed -i 's/		 templateScore/	 templateScore/g'
-##Remove  Rotifer COunt only llines - starting with tab
-sed -i '/^\t/d' AutoSet420fps_*.csv
--- Rplots/plotTracksLive.r
+++
@@ -1,8 +0,0 @@
-layout(matrix(1:2,ncol=1))
-while(T){
-    data=read.table("AutoSet_12-10-17_WTLiveFedRoti_148_007_tracks_1.csv",header=T);
-    plot(data$frameN[(nrow(data)-600):nrow(data)],(data$EyeLDeg-data$EyeRDeg)[(nrow(data)-600):nrow(data)],type='l',ylim=c(-15,80));
-    lines(data$frameN[(nrow(data)-600):nrow(data)],data$EyeLDeg[(nrow(data)-600):nrow(data)],col='green');
-    lines(data$frameN[(nrow(data)-600):nrow(data)],data$EyeRDeg[(nrow(data)-600):nrow(data)],col='red');
-    plot(data$frameN[(nrow(data)-600):nrow(data)],(data$AngleDeg)[(nrow(data)-600):nrow(data)],type='l',ylim=c(0,360));
-}
